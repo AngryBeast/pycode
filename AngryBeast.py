f79ffbfde3a038a67f1bd206a07ce592b45e1afb
AngryBeast <974635395@qq.com>
2022-05-14 17:14:13 +0800

    14 17:14

--- AngryBeast.py
+++ AngryBeast.py
@@ -138,18 +138,12 @@ class MyTcpThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         ADDRESS = ('127.0.0.1', 12345)
-        point_cloud_dataType = np.dtype([('num',int),('x',float),('y',float),('z',float)])
         self.BUFSIZ = 102400
-        self.point_cloud_data = np.zeros((9600),dtype = point_cloud_dataType)
+        self.deep_data = np.zeros((60,160),dtype = int)
         self.tcpClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcpClientSocket.connect(ADDRESS)
-        for i in range(9600):
-            self.point_cloud_data[i][0] = i
-            i += 1
 
     def run(self):
-        count = 0
-        i = 0
         while True:
             data, ADDR = self.tcpClientSocket.recvfrom(self.BUFSIZ)
             if not data:
@@ -157,26 +151,13 @@ class MyTcpThread(threading.Thread):
             revstr = data.decode().split('!')
             for num in revstr:
                 floatnum = re.findall(r"\d+\.?\d*",num)
-                if (len(floatnum) == 4):
+                if (len(floatnum) == 2):
                     tempNum = int(floatnum[0])
-                    if (tempNum == self.point_cloud_data[tempNum][0]):
-                        count += 1
-                        resultx = float(floatnum[1])
-                        resulty = float(floatnum[2])
-                        resultz = float(floatnum[3])
-                        self.point_cloud_data[tempNum] = (tempNum, resultx, resulty, resultz)
-                        #将数据传递出去？
-                        if (count == 9599):
-                            i+=1
-                            count = 0
-                            print(i,'done')
-                            #print(self.point_cloud_data[random.randint(0,9599)])
-
-                    else:
-                        count = 0
-                else:
-                    i = i
-                    #print('loss')
+                    deep_data[int(tempNum/160)][tempNum%160] = floatnum[1]
+                    lastNum = tempNum
+                    
+                    if (tempNum == 9599):
+                        print('done')
 
 def main():
     threadQt = MyQtThread()
--- TCPClient.py
+++ TCPClient.py
@@ -1,3 +1,63 @@
+# from socket import *
+# import numpy as np
+# import re
+
+
+
+# HOST = '127.0.0.1'
+# PORT = 12345
+# BUFSIZ = 1024
+# ADDRESS = (HOST, PORT)
+
+# point_cloud_dataType = np.dtype([('num',int),('x',float),('y',float),('z',float)])
+# point_cloud_data = np.zeros((9600),dtype = point_cloud_dataType)
+
+# for i in range(9600):
+#     point_cloud_data[i][0] = i
+#     i += 1
+
+# tcpClientSocket = socket(AF_INET, SOCK_STREAM)
+# tcpClientSocket.connect(ADDRESS)
+
+
+
+# print('start')
+# count = 0
+# i = 0
+# while True:
+#     data, ADDR = tcpClientSocket.recvfrom(BUFSIZ)
+#     if not data:
+#         break
+#     # revstr = data.decode().split('!')
+#     # for num in revstr:
+#     #     floatnum = re.findall(r"\d+\.?\d*",num)
+#     #     if (len(floatnum) == 4):
+#     #         tempNum = int(floatnum[0])
+#     #         if (tempNum == point_cloud_data[tempNum][0]):
+#     #             count += 1
+#     #             resultx = float(floatnum[1])
+#     #             resulty = float(floatnum[2])
+#     #             resultz = float(floatnum[3])
+#     #             point_cloud_data[tempNum] = (tempNum, resultx, resulty, resultz)
+
+#     #             # point_cloud_data[tempNum] = np.array([tempNum, resultx,resulty, resultz])           
+#     #             # point_cloud_data[tempNum][1] = float(floatnum[1]) 
+#     #             # point_cloud_data[tempNum][2] = float(floatnum[2]) 
+#     #             # point_cloud_data[tempNum][3] = float(floatnum[3]) 
+
+#     #             if (count == 9599):
+#     #                 i+=1
+#     #                 count = 0
+#     #                 print(i,'done')
+#     #         else:
+#     #             count = 0
+#     #     else:
+#     #         print('loss')
+
+# print("链接已断开！")
+# tcpClientSocket.close()
+
+
 from socket import *
 import numpy as np
 import re
@@ -9,30 +69,38 @@ PORT = 12345
 BUFSIZ = 1024
 ADDRESS = (HOST, PORT)
 
-point_cloud_dataType = np.dtype([('num',int),('x',float),('y',float),('z',float)])
-point_cloud_data = np.zeros((9600),dtype = point_cloud_dataType)
+#deep_dataType = np.dtype([('num',int),('data',int)])
+deep_data = np.zeros((60,160),dtype = int)
 
-for i in range(9600):
-    point_cloud_data[i][0] = i
-    i += 1
+# for i in range(60):
+#     for j in range(160):
+#         deep_data[i][j][0] = i * 160 + j
+
+
 
 tcpClientSocket = socket(AF_INET, SOCK_STREAM)
 tcpClientSocket.connect(ADDRESS)
 
-
-
 print('start')
 count = 0
-i = 0
+lastNum = -1
 while True:
     data, ADDR = tcpClientSocket.recvfrom(BUFSIZ)
     if not data:
         break
-    # revstr = data.decode().split('!')
-    # for num in revstr:
-    #     floatnum = re.findall(r"\d+\.?\d*",num)
-    #     if (len(floatnum) == 4):
-    #         tempNum = int(floatnum[0])
+    #print(data)
+    revstr = data.decode().split('!')
+    for num in revstr:
+        floatnum = re.findall(r"\d+\.?\d*",num)
+        if (len(floatnum) == 2):
+            tempNum = int(floatnum[0])
+            deep_data[int(tempNum/160)][tempNum%160] = floatnum[1]
+            lastNum = tempNum
+            # if (tempNum > 6000):
+            #     print(tempNum,':',floatnum[1],'!')
+            
+            if (tempNum == 9599):
+                print('done')
     #         if (tempNum == point_cloud_data[tempNum][0]):
     #             count += 1
     #             resultx = float(floatnum[1])
@@ -40,10 +108,6 @@ while True:
     #             resultz = float(floatnum[3])
     #             point_cloud_data[tempNum] = (tempNum, resultx, resulty, resultz)
 
-    #             # point_cloud_data[tempNum] = np.array([tempNum, resultx,resulty, resultz])           
-    #             # point_cloud_data[tempNum][1] = float(floatnum[1]) 
-    #             # point_cloud_data[tempNum][2] = float(floatnum[2]) 
-    #             # point_cloud_data[tempNum][3] = float(floatnum[3]) 
 
     #             if (count == 9599):
     #                 i+=1
@@ -53,6 +117,7 @@ while True:
     #             count = 0
     #     else:
     #         print('loss')
+
 
 print("链接已断开！")
 tcpClientSocket.close()
